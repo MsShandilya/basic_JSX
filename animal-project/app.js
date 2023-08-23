2495c28db528bc67d74d7f8801c600f592c824e8
MsShandilya <48158849+MsShandilya@users.noreply.github.com>
2023-08-24 00:52:32 +0530

    add a title
    ternary operator in JSX

--- animal-project/app.js
+++ animal-project/app.js
@@ -3,3 +3,8 @@ import {createRoot} from 'react-dom/client';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
+
+const title = '';
+const animalFacts =  <h1>{title === '' ? 'Click an animal for a fun fact' : title}</h1>;
+
+root.render(animalFacts);
