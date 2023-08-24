cb96410a343286b17a1f1a5ea11c14ab7ca64e3b
MsShandilya <48158849+MsShandilya@users.noreply.github.com>
2023-08-24 06:39:16 +0530

    adding images

--- animal-project/app.js
+++ animal-project/app.js
@@ -8,10 +8,23 @@ const root = createRoot(container);
 //attribute to store the ocean
 const background = <img className='background' alt='ocean'  src='/images/ocean.jpg' />;
 
+// imges placeholder
+const images = [];
+for(let animal in animals){
+  images.push(
+    <li>
+      <img  className='animal'  src = {animals[animal].image} aria-label={animal} role= 'button'/>
+    </li>
+    )
+}
+
 const title = '';
 const animalFacts =  (
     <div>
       {background}
+      <div className='animals'>
+        {images}
+      </div>
       <h1>
         {title === '' ? 'Click an animal for a fun fact' : title}
       </h1>
