1179c3dbaccce756e40d5cabfdd491a1adb64306
MsShandilya <48158849+MsShandilya@users.noreply.github.com>
2023-08-30 21:38:37 +0530

    react component passing
    react component passing

--- main-component/src/index.js
+++ main-component/src/index.js
@@ -1,7 +1,19 @@
 import { createRoot } from 'react-dom/client';
+import ProfilePage from './ProfilePage';
+
+
+const container = document.getElementById('root');
+
+const root = createRoot(container);
+root.render(<ProfilePage />);
+
+/*
+
 import Contact from './authorizationForm';
 
 const container = document.getElementById('root');
 
 const root = createRoot(container);
 root.render(<Contact />);
+
+*/
