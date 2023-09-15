af7cdc3c04254f071260658f990fb1971bb15ee3
MsShandilya <48158849+MsShandilya@users.noreply.github.com>
2023-09-15 08:28:10 +0530

    some screenshots
    some screenshots

--- sports-registration/src/eventList.js
+++ sports-registration/src/eventList.js
@@ -1,5 +1,5 @@
 // EventList.js
-import React from "react";
+import React, {useState, useEffect} from "react";
 import events from './mockData';
 import Card from './Card';
 import './eventList.css';
